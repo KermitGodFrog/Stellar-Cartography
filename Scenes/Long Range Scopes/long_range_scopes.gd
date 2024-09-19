028631a770ebc07f4c282a441751276e0ad90a0a
KermitGodFrog <kaden@REAL-Kadens-MacBook-Pro.local>
2024-09-19 13:12:07 +0800

    Camera rotation in long range scopes.

--- Scenes/Long Range Scopes/long_range_scopes.gd
+++ Scenes/Long Range Scopes/long_range_scopes.gd
@@ -4,12 +4,30 @@ extends Node3D
 @onready var camera = $camera_offset/camera
 @onready var camera_offset = $camera_offset
 
+func _unhandled_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
+			var viewport_size_y = get_viewport().get_visible_rect().size.y
+			var viewport_size_x = get_viewport().get_visible_rect().size.x
+			var mouse_pos_y = get_viewport().get_mouse_position().y
+			var mouse_pos_x = get_viewport().get_mouse_position().x
+			
+			if mouse_pos_y > (viewport_size_y - viewport_size_y / 10):
+				camera.transform.basis = camera.transform.basis.rotated(Vector3.LEFT, deg_to_rad(1))
+			if mouse_pos_y < (viewport_size_y / 10):
+				camera.transform.basis = camera.transform.basis.rotated(Vector3.RIGHT, deg_to_rad(1))
+			
+			if mouse_pos_x > (viewport_size_x - viewport_size_x / 10):
+				camera.transform.basis = camera.transform.basis.rotated(Vector3.DOWN, deg_to_rad(1))
+			if mouse_pos_x < (viewport_size_x / 10):
+				camera.transform.basis = camera.transform.basis.rotated(Vector3.UP, deg_to_rad(1))
+	pass
 
 
 
 
-
-
+func _physics_process(delta):
+	pass
 
 
 
