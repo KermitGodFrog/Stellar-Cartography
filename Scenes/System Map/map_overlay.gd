e2cf25d89dadc72c3d8110e93ac255fc813b8ab4
KermitGodFrog <kermitthegodfrog@gmail.com>
2024-11-16 16:47:38 +0800

    Reverting stylistic changes to map overlay due to bad performance.

--- Scenes/System Map/map_overlay.gd
+++ Scenes/System Map/map_overlay.gd
@@ -2,13 +2,10 @@ extends TextureRect
 
 @onready var frame_update = $frame_update
 var frame: int = 0
-var frame_update_time: float = 1.0
 
 
 func _physics_process(delta):
 	texture.region.position = Vector2(0, 112 * frame)
-	frame_update.wait_time = maxf(0.1, frame_update_time * delta)
-	print(frame_update.wait_time)
 	pass
 
 func _on_frame_update_timeout():
