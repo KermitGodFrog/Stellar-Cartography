0385f4e32214a8914b549201d4786c4fa96e26a0
KermitGodFrog <kermitthegodfrog@gmail.com>
2025-01-23 13:48:07 +0800

    New rules (final before compiling next update). Loading progress bar now uses linear interpolation.

--- Scenes/Loading Screen/loading_screen.gd
+++ Scenes/Loading Screen/loading_screen.gd
@@ -2,6 +2,14 @@ extends Control
 
 @onready var progress_fill = $progress_fill
 
+var ghost_progress: float = 0.0
+var estimated_progress: float = 0.0
+
 func update_progress(new_progress: float):
-	progress_fill.set_value(new_progress)
+	ghost_progress = new_progress
+	pass
+
+func _process(delta):
+	estimated_progress = lerpf(estimated_progress, ghost_progress, delta)
+	progress_fill.set_value(estimated_progress)
 	pass
