7a86da703886e8e03a32650ca97bea0668874d00
KermitGodFrog <kermitthegodfrog@gmail.com>
2025-04-23 16:41:50 +0800

    Attempted changes to the music linear volume target system that induce a state of hysterisis for music volume, which was what I was trying to avoid in the first place.

--- Scenes/Game/game.gd
+++ Scenes/Game/game.gd
@@ -244,7 +244,8 @@ func _physics_process(delta):
 	audio_visualizer.set("saved_audio_profiles", world.player.saved_audio_profiles)
 	dialogue_manager.set("player", world.player)
 	lrs_bestiary.set("discovered_entities_matrix", world.player.discovered_entities)
-	audio_handler.set("audio_visualizer_visible", $audio_visualizer_window.is_visible()) # for music ducking, but there shouldnt be GREEN here so fix later!
+	audio_handler.enable_music_votes.append(! $audio_visualizer_window.is_visible()) #there should not be green here
+	audio_handler.enable_music_votes.append(! countdown_processor != null)
 	
 	game_data.player_weirdness_index = world.player.weirdness_index #really hacky solution which should not have been done this way but im too tired to change the entire game now to accomodate it.
 	
