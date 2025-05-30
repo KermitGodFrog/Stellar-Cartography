9ffe6130dd52f301f67f8f46fb03eaf905e4fbae
KermitGodFrog <kermitthegodfrog@gmail.com>
2025-05-30 19:48:46 +0800

    Fixed system_map.gd and bodyAPI usage of defunct metadata keys. Found little issues with the updates besides Stellar-Cartographer #77. Still need to check dialogueManager seed usage.

--- Data/BodyAPIs/spaceAnomalyBodyAPI.gd
+++ Data/BodyAPIs/spaceAnomalyBodyAPI.gd
@@ -2,6 +2,6 @@ extends glintBodyAPI
 class_name spaceAnomalyBodyAPI
 
 func is_SA_valid() -> bool:
-	if (metadata.get("is_space_anomaly_available", true) == true):
+	if (metadata.get("space_anomaly_available", true) == true):
 		return true
 	return false
