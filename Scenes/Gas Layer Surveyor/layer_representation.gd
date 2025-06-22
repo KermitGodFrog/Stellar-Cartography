6fff283cfc0ceac1cdf82819c1e9ff05b15f0a55
KermitGodFrog <kermitthegodfrog@gmail.com>
2025-06-22 13:18:37 +0800

    Improvements to layer representations. Now shuffled when constructing the selection screen. Now can be moved between columns.

--- Scenes/Gas Layer Surveyor/layer_representation.gd
+++ Scenes/Gas Layer Surveyor/layer_representation.gd
@@ -1,25 +1,24 @@
 extends TextureButton
+enum LISTS {HIERACHY, CHOICES}
+enum ACTIONS {SWITCH_COLUMN, VIEW_IN_ENCYCLOPEDIA}
 
-var current_state: STATES:
-	get = get_state, set = set_state
-enum STATES {CHOICE, HIERACHY}
-func get_state() -> STATES:
-	return current_state
-func set_state(value) -> void:
-	current_state = value
+signal activated(_tag: String, _list: LISTS, _action: ACTIONS)
 
-
+var current_list: LISTS
 
 var tag: String = String()
 var data: Dictionary = Dictionary()
 
-func initialize(_tag: String, _data: Dictionary) -> void:
+func initialize(_tag: String, _data: Dictionary, _list: LISTS) -> void:
 	tag = _tag
 	data = _data
+	current_list = _list
 	pass
 
 func _ready() -> void:
 	tooltip_title = tag.to_upper()
+	texture_normal.gradient.set_color(0, data.get("bg_color", Color.WHITE))
+	texture_normal.gradient.set_color(1, data.get("fog_albedo", Color.WHITE))
 	pass
 
 
@@ -27,8 +26,17 @@ func _ready() -> void:
 #yoinking this from system_map
 @onready var custom_tooltip = preload("res://Scenes/Custom Tooltip/custom_tooltip.tscn")
 var tooltip_title: String
-
 func _make_custom_tooltip(for_text):
 	var custom_tooltip_instance = custom_tooltip.instantiate()
 	custom_tooltip_instance.initialize(tooltip_title, for_text)
 	return custom_tooltip_instance
+
+
+
+func _on_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.is_pressed():
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			emit_signal("activated", tag, current_list, ACTIONS.SWITCH_COLUMN)
+		if event.button_index == MOUSE_BUTTON_RIGHT:
+			emit_signal("activated", tag, current_list, ACTIONS.VIEW_IN_ENCYCLOPEDIA)
+	pass
--- Scenes/Gas Layer Surveyor/selection_screen.gd
+++ Scenes/Gas Layer Surveyor/selection_screen.gd
@@ -1,4 +1,6 @@
 extends PanelContainer
+enum LISTS {HIERACHY, CHOICES}
+enum ACTIONS {SWITCH_COLUMN, VIEW_IN_ENCYCLOPEDIA}
 
 @onready var hierachy_list = $margin/tabs/REPORT/scroll/hierachy_texture/hierachy_margin/hierachy_list
 @onready var choices_list = $margin/tabs/REPORT/scroll/choices_list
@@ -14,16 +16,48 @@ func initialize(_current_layers: PackedStringArray) -> void:
 	for c in choices_list.get_children(): c.queue_free()
 	for c in hierachy_list.get_children(): c.queue_free()
 	
-	for layer in _current_layers:
-		var layer_instance = layer_representation.instantiate()
-		layer_instance.initialize(layer, _layer_data.get(layer))
-		choices_list.add_child(layer_instance)
-		
-		
+	var converted_layers = Array(_current_layers)
+	converted_layers.shuffle()
 	
-	
-	
-	
-	
-	
+	for tag in converted_layers:
+		add_layer_instance(tag, LISTS.CHOICES)
+	pass
+
+
+
+func add_layer_instance(tag: String, list: LISTS):
+	var layer_instance = layer_representation.instantiate()
+	layer_instance.connect("activated", _on_layer_instance_activated)
+	layer_instance.initialize(tag, _layer_data.get(tag), list)
+	match list:
+		LISTS.CHOICES:
+			choices_list.add_child(layer_instance)
+		LISTS.HIERACHY:
+			hierachy_list.add_child(layer_instance)
+	pass
+
+func remove_layer_instance(tag: String, list: LISTS):
+	match list:
+		LISTS.CHOICES:
+			for c in choices_list.get_children():
+				if c.tag == tag:
+					c.queue_free()
+		LISTS.HIERACHY:
+			for c in hierachy_list.get_children():
+				if c.tag == tag:
+					c.queue_free()
+	pass
+
+func _on_layer_instance_activated(tag: String, list: LISTS, action: ACTIONS):
+	match action:
+		ACTIONS.SWITCH_COLUMN:
+			match list:
+				LISTS.CHOICES:
+					remove_layer_instance(tag, list)
+					add_layer_instance(tag, LISTS.HIERACHY)
+				LISTS.HIERACHY:
+					remove_layer_instance(tag, list)
+					add_layer_instance(tag, LISTS.CHOICES)
+		ACTIONS.VIEW_IN_ENCYCLOPEDIA:
+			pass
 	pass
