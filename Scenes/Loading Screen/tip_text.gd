06a0386b43c2d585b14f1f998e4bb6ff3873b707
KermitGodFrog <kermitthegodfrog@gmail.com>
2025-09-22 18:19:56 +0800

    Replaced abyss_intermission.wav with a shorter version. Added a few tips to tip_text.gd.

--- Scenes/Loading Screen/tip_text.gd
+++ Scenes/Loading Screen/tip_text.gd
@@ -2,14 +2,11 @@ extends Label
 
 const tips = [
 	"To quick-orbit a body, right click on it in the SYSTEM LIST.",
-	"If you don't find ways to repair while exploring, you will always gain hull deterioration before reaching the next civilized system."
-	
-	
-	
-	
-	
+	"If you don't find ways to repair while exploring, you will always gain hull deterioration before reaching the next civilized system.",
+	"Click on a body on the SYSTEM MAP to quick-lock.",
+	"Right click on a body on the SYSTEM MAP to quick-orbit.",
+	"Note the 'MARKET PRICE' at all space stations in a system before selling your exploration data."
 ]
-
 
 func _ready() -> void:
 	set_text(tips.pick_random())
