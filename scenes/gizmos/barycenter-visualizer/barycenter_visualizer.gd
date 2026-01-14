badfb3b130e435b07cb809fa14d0d45a44e581f7
KermitGodFrog <kermitthegodfrog@gmail.com>
2026-01-14 17:31:27 +0800

    glintBodyAPIs now appear on barycenter_visualizer. Minor performance enhancements to barycenter_visualizer.

--- scenes/gizmos/barycenter-visualizer/barycenter_visualizer.gd
+++ scenes/gizmos/barycenter-visualizer/barycenter_visualizer.gd
@@ -6,7 +6,10 @@ var TUTORIAL_INGRESS_OVERRIDE: bool = false
 var TUTORIAL_OMISSION_OVERRIDE: bool = false
 
 var system: starSystemAPI
-var locked_body_identifier: int
+var locked_body_identifier: int:
+	set(value):
+		locked_body_identifier = value
+		_on_locked_body_identifier_changed(value)
 
 var _ping_length: int = 0 #game.gd _on_sonar_values_changed
 var _ping_direction: Vector2 = Vector2.ZERO
@@ -14,13 +17,22 @@ var _player_position: Vector2 = Vector2.ZERO #game.gd _physics_process
 
 var point_count: int = 20
 var radius: int = 50
+
 var points: Dictionary = {}
 var pingable_points: Dictionary = {}
+var glint_points: Dictionary = {}
+var pingable_glint_points: Dictionary = {}
 
-func _physics_process(_delta):
+@onready var glint_texture = preload("uid://7rpr8t50r5bs")
+
+func _on_refresh_timeout() -> void:
 	points.clear()
 	pingable_points.clear()
+	glint_points.clear()
+	pingable_glint_points.clear()
+	
 	var locked_body = system.get_body_from_identifier(locked_body_identifier)
+	
 	for i in point_count:
 		var theta = (360 / point_count - 1) * i
 		var x = radius * cos(theta)
@@ -57,7 +69,21 @@ func _physics_process(_delta):
 				if _player_position.distance_to(body.position) < _ping_length:
 					if _ping_direction != Vector2.ZERO:
 						pingable_points[closest_point] = true
-	
+		elif body is glintBodyAPI and body.get_identifier() != locked_body_identifier:
+			if locked_body:
+				var dir = locked_body.position.direction_to(body.position)
+				var closest_point = get_closest_point_to_direction(dir)
+				glint_points[closest_point] = 10.0
+				
+				if _player_position.distance_to(body.position) < _ping_length:
+					if _ping_direction != Vector2.ZERO:
+						pingable_glint_points[closest_point] = true
+	pass
+
+
+
+func _physics_process(_delta):
+	var locked_body = system.get_body_from_identifier(locked_body_identifier)
 	if locked_body: 
 		if locked_body.is_known(): locked_body_label.set_text(locked_body.get_display_name())
 		elif locked_body.is_theorised_not_known(): locked_body_label.set_text("Unknown")
@@ -81,7 +107,21 @@ func _draw():
 			draw_circle(point, points.get(point), Color.RED)
 		else:
 			draw_circle(point, points.get(point), Color.DARK_RED)
+	for glint_point in glint_points:
+		var texture_size = glint_points.get(glint_point)
+		if pingable_glint_points.get(glint_point, false) == true:
+			draw_texture_rect(glint_texture, global_data.get_offset_rect2(glint_point, texture_size, texture_size), false, Color.RED.darkened(0.25))
+		else:
+			draw_texture_rect(glint_texture, global_data.get_offset_rect2(glint_point, texture_size, texture_size), false, Color.DARK_RED.darkened(0.25))
 	pass
 
 func get_screen_centre():
 	return (get_viewport_rect().size / 2)
+
+func _on_locked_body_identifier_changed(_new_identifier: int) -> void:
+	points.clear()
+	pingable_points.clear()
+	glint_points.clear()
+	pingable_glint_points.clear()
+	_on_refresh_timeout()
+	pass
