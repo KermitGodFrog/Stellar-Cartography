48b4fa16610a4708fe6f6fe1e1fbc390841a14e6
KermitGodFrog <kermitthegodfrog@gmail.com>
2026-03-08 13:31:27 +0800

    Reduced LIDAR suite dB from 0.0 to -12.0 for exploring and intercepting units. Replaced 'unit_available' in info tab metadata exclusion with 'ship_available'.

--- data/game/bodyAPIs/unitAPIs/interceptingUnitAPI.gd
+++ data/game/bodyAPIs/unitAPIs/interceptingUnitAPI.gd
@@ -176,7 +176,7 @@ func switch_task(override_task = null) -> void:
 			task_clock.start(global_data.get_randf(20.0,60.0))
 		TASKS.USE_LIDAR:
 			task_clock.start(15.0)
-			emit_signal("play_sound", "res://sound/game/bodyAPIs/unitAPIs/LIDAR_unit_suite.tres", 0.0, "SFX")
+			emit_signal("play_sound", "res://sound/game/bodyAPIs/unitAPIs/LIDAR_unit_suite.tres", -12.0, "SFX")
 			course_to_position(position)
 		TASKS.MOVE_TO_LIDAR:
 			if last_player_position != Vector2.ZERO:
