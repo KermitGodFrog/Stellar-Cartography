bb5ad5966f725a140bc6ae988b95a6ce5c09a318
KermitGodFrog <kermitthegodfrog@gmail.com>
2026-04-27 18:05:51 +0800

    Popups now set their position in _process (failed to make it work in the editor though). Added a new popup for tutorialDiscoverPrelude.

--- instantiated-scenes/info-popups/closable_popup.gd
+++ instantiated-scenes/info-popups/closable_popup.gd
@@ -1,6 +1,5 @@
 extends "res://instantiated-scenes/info-popups/info_popup.gd"
 
-
 func set_popup_state(_state: objectiveAPI.STATES) -> void:
 	match _state:
 		objectiveAPI.STATES.NONE:
--- instantiated-scenes/info-popups/info_popup.gd
+++ instantiated-scenes/info-popups/info_popup.gd
@@ -13,16 +13,25 @@ var last_popup_state: int = -1 #below STATES constituting 0, 1, 2
 ##if applicable
 @export var close_button_path: String
 
+
+
+#primary
 func _ready() -> void:
 	get_node(NodePath(text_box_path)).set_text(info)
 	if text_box_size != Vector2.ZERO:
 		get_node(NodePath(text_box_path)).set_custom_minimum_size(text_box_size)
-	connect("visibility_changed", _on_visibility_changed)
-	
 	if close_button_path.length() > 0:
 		get_node(NodePath(close_button_path)).connect("pressed", _on_close_button_pressed)
 	pass
 
+func _process(_delta: float) -> void:
+	if track != null:
+		set_global_position((get_node(track).get_global_position() + (get_node(track).get_size() / 2) - (get_size() / 2)) + track_offset)
+	pass
+
+
+
+#signals
 func _on_active_objectives_changed(active_objectives: Array[objectiveAPI]):
 	for obj in active_objectives:
 		if obj.get_wID() == target_objective_wID:
@@ -32,11 +41,6 @@ func _on_active_objectives_changed(active_objectives: Array[objectiveAPI]):
 				last_popup_state = obj_state
 	pass
 
-func _on_visibility_changed() -> void:
-	if track != null:
-		set_global_position((get_node(track).get_global_position() + (get_node(track).get_size() / 2) - (get_size() / 2)) + track_offset)
-	pass
-
 func _on_close_button_pressed() -> void:
 	hide()
 	get_tree().call_group_flags(SceneTree.GROUP_CALL_DEFERRED | SceneTree.GROUP_CALL_UNIQUE, "eventsHandler", "speak", self, "info_popup_close_press", target_objective_wID)
@@ -44,7 +48,6 @@ func _on_close_button_pressed() -> void:
 
 
 
-
-
+#misc
 func set_popup_state(_state: objectiveAPI.STATES) -> void: # this is whats modified by inherited classes
 	pass
