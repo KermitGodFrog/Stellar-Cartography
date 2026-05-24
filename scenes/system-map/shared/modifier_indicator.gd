d6e90a97eaff3b5c88dc6dd304d321eb421955d0
KermitGodFrog <kermitthegodfrog@gmail.com>
2026-05-24 16:49:56 +0800

    Added custom tooltip support for proximity blinker and the two relationship symbols.

--- scenes/system-map/shared/modifier_indicator.gd
+++ scenes/system-map/shared/modifier_indicator.gd
@@ -1,13 +1,4 @@
-extends PanelContainer
-
-@onready var custom_tooltip = preload("uid://cer7wk3ixuyb6")
-@export var tooltip_title: String
-
-func _make_custom_tooltip(for_text):
-	var custom_tooltip_instance = custom_tooltip.instantiate()
-	custom_tooltip_instance.initialize(tooltip_title, for_text)
-	return custom_tooltip_instance
-
+extends "res://scenes/system-map/custom_tooltip_control.gd"
 
 @onready var title_label = $title_label
 var associated_id
