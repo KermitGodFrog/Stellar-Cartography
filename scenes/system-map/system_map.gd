7aba2d45fbcd0fb8fa67d9345faf141989f6f708
KermitGodFrog <kermitthegodfrog@gmail.com>
2026-05-25 18:20:13 +0800

    Changed glintBodyAPI texture colour on system_map to provide contrast with the player. Changed the distance to quick-orbit and quick-lock bodies on the system_map to below 1 + the standard size (zoom_multiplier * 10).

--- scenes/system-map/system_map.gd
+++ scenes/system-map/system_map.gd
@@ -562,9 +562,12 @@ func update_proximity_blinker(_active: bool) -> void:
 
 
 func _unhandled_input(event):
+	var zoom_multiplier = pow(camera.zoom.length(), -1.0)
+	var standard_size = zoom_multiplier * 10.0
+	
 	if event.is_action_pressed("SC_INTERACT2_RIGHT_MOUSE"):
 		var closest_body = global_data.get_closest_body(system.bodies, get_global_mouse_position())
-		if get_global_mouse_position().distance_to(closest_body.position) < (1 + pow(camera.zoom.length(), -0.5)) \
+		if get_global_mouse_position().distance_to(closest_body.position) < (1 + standard_size) \
 		and (not closest_body.is_not_known_or_is_hidden()) \
 		and (not closest_body is unitBodyAPI):
 			emit_signal("updatedLockedBody", closest_body)
@@ -586,7 +589,7 @@ func _unhandled_input(event):
 	
 	if event.is_action_pressed("SC_INTERACT1_LEFT_MOUSE"):
 		var closest_body = global_data.get_closest_body(system.bodies, get_global_mouse_position())
-		if get_global_mouse_position().distance_to(closest_body.position) < (1 + pow(camera.zoom.length(), -0.5)) \
+		if get_global_mouse_position().distance_to(closest_body.position) < (1 + standard_size) \
 		and (not closest_body.is_not_known_or_is_hidden()):
 			emit_signal("updatedLockedBody", closest_body)
 			locked_body = closest_body
@@ -813,7 +816,7 @@ func draw_map():
 		#batching entity icons:
 		
 		if body is glintBodyAPI and body.is_known():
-			entity_texture.draw_rect(get_canvas_item(), global_data.get_offset_rect2(body.position, standard_size * 1.5, standard_size * 1.5), false)
+			entity_texture.draw_rect(get_canvas_item(), global_data.get_offset_rect2(body.position, standard_size * 1.5, standard_size * 1.5), false, Color.LIGHT_GRAY)
 	
 	for body in system.bodies:
 		
