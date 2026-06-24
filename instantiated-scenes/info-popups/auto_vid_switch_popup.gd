fe24801cbd583103f6cb4842b1d240f139b16304
KermitGodFrog <kermitthegodfrog@gmail.com>
2026-06-24 14:58:31 +0800

    Added videos for tutorialMarkIngress and tutorialMarkPrelude objectives. Tutorial info popups for tutorialMarkIngress and tutorialMarkPrelude are now auto_vid_switch_popup.

--- instantiated-scenes/info-popups/auto_vid_switch_popup.gd
+++ instantiated-scenes/info-popups/auto_vid_switch_popup.gd
@@ -1,20 +1,28 @@
 extends "res://instantiated-scenes/info-popups/info_popup.gd"
 
-@export var video_path: String
-@export var video_stream: VideoStream
-@export var switch_button_path: String
+@onready var video = $mid_panel_fluid/mid_panel/margin/vid
+@onready var switch_button = $mid_panel_fluid/switch_button
+
+@export var video_file: String
 
 func _ready() -> void:
 	if text_box_size != Vector2.ZERO:
-		get_node(NodePath(video_path)).set_custom_minimum_size(text_box_size)
-	if switch_button_path.length() > 0:
-		get_node(NodePath(switch_button_path)).connect("pressed", _on_switch_button_pressed)
+		video.set_custom_minimum_size(text_box_size)
+	switch_button.connect("pressed", _on_switch_button_pressed)
+	
+	var stream = VideoStreamTheora.new()
+	stream.set_file(video_file)
+	video.set_stream(stream)
 	super()
 	pass
 
 func _on_switch_button_pressed() -> void:
 	get_node(NodePath(text_box_path)).visible = !get_node(NodePath(text_box_path)).visible
-	get_node(NodePath(video_path)).visible = !get_node(NodePath(video_path)).visible
+	video.visible = !video.visible
+	if video.visible:
+		video.play()
+	else:
+		video.stop()
 	pass
 
 func set_popup_state(_state: objectiveAPI.STATES) -> void:
