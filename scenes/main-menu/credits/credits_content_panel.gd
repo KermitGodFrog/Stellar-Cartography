080944a6b4a59cf0ce5abe64070e9c216a4264b2
KermitGodFrog <kermitthegodfrog@gmail.com>
2026-06-25 14:59:05 +0800

    Updated credits popup and the style of the credits popup (main menu). Updated main menu title font.

--- scenes/main-menu/credits/credits_content_panel.gd
+++ scenes/main-menu/credits/credits_content_panel.gd
@@ -6,10 +6,8 @@ extends PanelContainer
 
 @export var title: String
 @export_multiline var text: String
-@export var panel_color: Color = Color("181818")
 
 func _ready() -> void:
 	title_label.set_text(title)
 	text_label.append_text(text)
-	set("theme_override_Styles/panel/bg_color", panel_color)
 	pass
