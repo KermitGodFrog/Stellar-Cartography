9ae5ad214c3c5dffee1667a3019b182f3d50aace
KermitGodFrog <kermitthegodfrog@gmail.com>
2026-07-05 14:31:21 +0800

    Implemented video titles for auto_vid_switch_popup.

--- instantiated-scenes/info-popups/auto_vid_switch_popup.gd
+++ instantiated-scenes/info-popups/auto_vid_switch_popup.gd
@@ -4,14 +4,18 @@ extends "res://instantiated-scenes/info-popups/info_popup.gd"
 @onready var video_container = $mid_panel_fluid/mid_panel/margin/vid_container
 @onready var switch_button = $mid_panel_fluid/switch_button
 @onready var switch_button_flash = $switch_button_flash
+@onready var vid_title = $mid_panel_fluid/mid_panel/margin/vid_title
 
 @export var video_file: String
+@export_multiline var video_title_text: String
 @export_range(0.001, 10.0, 0.001) var video_aspect_ratio: float = 1.0
 @export var flash_switch_button_until_pressed: bool = false
 
 var current_color: Color = Color.WHITE
 
 func _ready() -> void:
+	vid_title.set_text(video_title_text)
+	
 	switch_button.connect("pressed", _on_switch_button_pressed)
 	
 	video_container.set_ratio(video_aspect_ratio)
@@ -37,6 +41,7 @@ func _on_switch_button_pressed() -> void:
 	
 	get_node(NodePath(text_box_path)).visible = !get_node(NodePath(text_box_path)).visible
 	video_container.visible = !video_container.visible
+	vid_title.visible = !vid_title.visible
 	if video_container.visible:
 		video.play()
 		switch_button.set_text("VIEW TEXT")
