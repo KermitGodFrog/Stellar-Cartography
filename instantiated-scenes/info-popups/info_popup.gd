64474555cac3630e9bed32f584a88bc7c64db268
KermitGodFrog <kermitthegodfrog@gmail.com>
2026-07-05 14:23:18 +0800

    Implemented free movement of info popups (that are tracking a node) without blocking events for other UI elements.

--- instantiated-scenes/info-popups/info_popup.gd
+++ instantiated-scenes/info-popups/info_popup.gd
@@ -3,6 +3,7 @@ extends CenterContainer
 @export var target_objective_wID: String
 var last_popup_state: int = -1 #below STATES constituting 0, 1, 2
 
+@export_node_path() var mid_panel
 @export var text_box_path: String
 @export var text_box_size: Vector2
 @export_node_path() var track
@@ -17,9 +18,15 @@ var last_popup_state: int = -1 #below STATES constituting 0, 1, 2
 @export var close_button_path: String
 
 var manual_offset: Vector2 = Vector2.ZERO 
+var dragging: bool = false
 
 #primary
 func _ready() -> void:
+	if track != null:
+		get_node(mid_panel).mouse_filter = Control.MOUSE_FILTER_PASS
+		get_node(mid_panel).mouse_default_cursor_shape = Control.CURSOR_MOVE
+	get_node(mid_panel).connect("gui_input", _on_mid_panel_gui_input)
+	
 	get_node(NodePath(text_box_path)).set_text(global_data.replace_keybind_references(info))
 	if text_box_size != Vector2.ZERO:
 		get_node(NodePath(text_box_path)).set_custom_minimum_size(text_box_size)
@@ -28,8 +35,6 @@ func _ready() -> void:
 	if background_path.length() > 0:
 		get_node(NodePath(background_path)).set_color(Color("2e2e2e", background_alpha))
 	pass
-
-
 
 func _process(_delta: float) -> void:
 	if track != null:
@@ -53,6 +58,16 @@ func _on_close_button_pressed() -> void:
 	get_tree().call_group_flags(SceneTree.GROUP_CALL_DEFERRED | SceneTree.GROUP_CALL_UNIQUE, "eventsHandler", "speak", self, "info_popup_close_press", target_objective_wID)
 	pass
 
+func _on_mid_panel_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			dragging = true
+		else:
+			dragging = false
+	elif event is InputEventMouseMotion:
+		if dragging:
+			manual_offset += event.relative
+	pass
 
 #misc
 func set_popup_state(_state: objectiveAPI.STATES) -> void: # this is whats modified by inherited classes
