a501818f4f0342935f7dc66628514540fb4f9def
KermitGodFrog <kermitthegodfrog@gmail.com>
2026-07-08 12:36:29 +0800

    Improved auto_vid_switch_popup video title rendering. Implemented play button for auto_vid_switch_popup.

--- instantiated-scenes/info-popups/auto_vid_switch_popup.gd
+++ instantiated-scenes/info-popups/auto_vid_switch_popup.gd
@@ -1,10 +1,12 @@
 extends "res://instantiated-scenes/info-popups/info_popup.gd"
 
-@onready var video = $mid_panel_fluid/mid_panel/margin/vid_container/vid
-@onready var video_container = $mid_panel_fluid/mid_panel/margin/vid_container
+@onready var video = $mid_panel_fluid/mid_panel/margin/vid_scroll/vid_container/vid
+@onready var video_container = $mid_panel_fluid/mid_panel/margin/vid_scroll/vid_container
 @onready var switch_button = $mid_panel_fluid/switch_button
 @onready var switch_button_flash = $switch_button_flash
-@onready var vid_title = $mid_panel_fluid/mid_panel/margin/vid_title
+@onready var vid_title = $mid_panel_fluid/mid_panel/margin/vid_scroll/vid_title
+@onready var vid_scroll = $mid_panel_fluid/mid_panel/margin/vid_scroll
+@onready var play_button = $mid_panel_fluid/mid_panel/margin/vid_scroll/vid_container/vid/play_button
 
 @export var video_file: String
 @export_multiline var video_title_text: String
@@ -27,19 +29,12 @@ func _ready() -> void:
 	video.set_stream(stream)
 	
 	super()
-	
-	await get_tree().create_timer(1.0, true).timeout
-	
-	if start_on_video:
-		_on_switch_button_pressed()
-	
-	if flash_switch_button_until_pressed:
-		switch_button_flash.oscillate_property(self, "current_color", Color.WHITE, Color.YELLOW, 20, 10, true)
 	pass
 
 func _process(delta: float) -> void:
 	switch_button.set("theme_override_colors/font_color", current_color)
 	switch_button.set("theme_override_colors/icon_normal_color", current_color)
+	play_button.visible = not video.is_playing()
 	
 	super(delta) #forgot this and spent AGES debugging it
 	pass
@@ -50,11 +45,10 @@ func _on_switch_button_pressed() -> void:
 	reset_vid_container_and_title_sizes()
 	
 	get_node(NodePath(text_box_path)).visible = !get_node(NodePath(text_box_path)).visible
-	video_container.visible = !video_container.visible
-	vid_title.visible = !vid_title.visible
+	vid_scroll.visible = !vid_scroll.visible
 	
 	if video_container.visible:
-		video.play()
+		video.stop()
 		switch_button.set_text("VIEW TEXT")
 	else:
 		video.stop()
@@ -64,16 +58,26 @@ func _on_switch_button_pressed() -> void:
 func set_popup_state(_state: objectiveAPI.STATES) -> void:
 	match _state:
 		objectiveAPI.STATES.NONE:
-			_on_switch_button_pressed()
-			_on_switch_button_pressed()
 			show()
+			
+			await get_tree().create_timer(1.0, true).timeout
+			
+			if start_on_video:
+				_on_switch_button_pressed()
+			
+			if flash_switch_button_until_pressed:
+				switch_button_flash.oscillate_property(self, "current_color", Color.WHITE, Color.YELLOW, 20, 10, true)
 		_:
 			hide()
 	pass
 
 func reset_vid_container_and_title_sizes() -> void:
-	video_container.set_custom_minimum_size(get_node(NodePath(text_box_path)).get_size())
-	video_container.set_size(get_node(NodePath(text_box_path)).get_size())
-	vid_title.set_custom_minimum_size(Vector2(get_node(NodePath(text_box_path)).get_size().x, 0))
-	vid_title.set_size(get_node(NodePath(text_box_path)).get_size())
+	vid_scroll.set_custom_minimum_size(get_node(NodePath(text_box_path)).get_size())
+	vid_scroll.set_size(get_node(NodePath(text_box_path)).get_size())
+	#vid_title.set_custom_minimum_size(Vector2(get_node(NodePath(text_box_path)).get_size().x, 0))
+	#vid_title.set_size(get_node(NodePath(text_box_path)).get_size())
+	pass
+
+func _on_play_button_pressed() -> void:
+	video.play()
 	pass
