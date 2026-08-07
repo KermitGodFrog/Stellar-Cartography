d9fe237d030be0b18f8630dae8b1d916dd6d19b1
KermitGodFrog <kermitthegodfrog@gmail.com>
2026-08-07 19:11:51 +0800

    If a planet decides to not generate sub bodies, its iterations are still added to post_gen_location_candidates. This makes post gen bodies appear orbiting planets far more often (which is cool), however at the moment it also makes space anomalies extremely common (since all post_gen_location_candidates are possible spawn locations and there is like 4x the amount of locations by doing this), and orbits tend to be stupid because there are no distance limits for what iterations can be added to post gen.

--- data/game/starSystemAPI.gd
+++ data/game/starSystemAPI.gd
@@ -641,7 +641,7 @@ func addRandomWeightedPlanetAtIteration(hook_identifier: int, i: int, remaining:
 				has_missing_GL = true
 				gas_layers_sum = global_data.get_randi(4, 9)
 	
-	var new_planet = addOrbitBody(
+	var new_planet_id := addOrbitBody(
 		planetBodyAPI.new(),
 		BODY_TYPES.PLANET,
 		identifier_count,
@@ -653,11 +653,15 @@ func addRandomWeightedPlanetAtIteration(hook_identifier: int, i: int, remaining:
 		{"mass": (mass / 333000), "surface_color": color, "current_variation": planetBodyAPI.VARIATIONS.values().pick_random(), "layers": gas_layers_sum},
 		{"planet_classification": planet_classification, "planet_type": planet_type, "value": value, "iterations": (hook.metadata.get("iterations") / 2), "planetary_anomaly": has_planetary_anomaly, "planetary_anomaly_available": is_planetary_anomaly_available, "seed": randi(), "missing_AO": has_missing_AO, "missing_GL": has_missing_GL}
 	)
+	var new_planet: planetBodyAPI = get_body_from_identifier(new_planet_id)
+	new_planet.rotation = deg_to_rad(global_data.get_randf(0,360))
 	
-	get_body_from_identifier(new_planet).rotation = deg_to_rad(global_data.get_randf(0,360))
-	
+	# \/ tries to add moons and adds remaining locations to post_gen OR just adds everything to post gen
 	if generate_sub_bodies:
-		generateRandomWeightedPlanets(new_planet, PA_chance_per_planet, missing_AO_chance_per_planet, missing_GL_chance_per_relevant_planet)
+		generateRandomWeightedPlanets(new_planet_id, PA_chance_per_planet, missing_AO_chance_per_planet, missing_GL_chance_per_relevant_planet)
+	else:
+		for iter in new_planet.metadata.get("iterations", 0):
+			post_gen_location_candidates.append([new_planet_id, iter])
 	pass
 func addRandomWeightedAsteroidBeltAtIteration(hook_identifier: int, i: int) -> int: # has a chance to fail if orbit_distance > belt_width
 	var hook = get_body_from_identifier(hook_identifier)
