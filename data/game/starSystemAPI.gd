KermitGodFrog/Stellar-Cartography

commit d9fe237d030be0b18f8630dae8b1d916dd6d19b1

If a planet decides to not generate sub bodies, its iterations are still added to post_gen_location_candidates. This makes post gen bodies appear orbiting planets far more often (which is cool), however at the moment it also makes space anomalies extremely common (since all post_gen_location_candidates are possible spawn locations and there is like 4x the amount of locations by doing this), and orbits tend to be stupid because there are no distance limits for what iterations can be added to post gen.

data/game/starSystemAPI.gd
@@ -641,7 +641,7 @@ func addRandomWeightedPlanetAtIteration(hook_identifier: int, i: int, remaining:
 				has_missing_GL = true
 				gas_layers_sum = global_data.get_randi(4, 9)
 	
-	var new_planet = addOrbitBody(
+	var new_planet_id := addOrbitBody(
 		planetBodyAPI.new(),
 		BODY_TYPES.PLANET,
 		identifier_count,
@@ -653,11 +653,15 @@ func addRandomWeightedPlanetAtIteration(hook_identifier: int, i: int, remaining:
 		{"mass": (mass / 333000), "surface_color": color, "current_variation": planetBodyAPI.VARIATIONS.values().pick_random(), "layers": gas_layers_sum},
 		{"planet_classification": planet_classification, "planet_type": planet_type, "value": value, "iterations": (hook.metadata.get("iterations") / 2), "planetary_anomaly": has_planetary_anomaly, "planetary_anomaly_available": is_planetary_anomaly_available, "seed": randi(), "missing_AO": has_missing_AO, "missing_GL": has_missing_GL}
 	)
+	var new_planet: planetBodyAPI = get_body_from_identifier(new_planet_id)
+	new_planet.rotation = deg_to_rad(global_data.get_randf(0,360))
 	
-	get_body_from_identifier(new_planet).rotation = deg_to_rad(global_data.get_randf(0,360))
-	
+	# \/ tries to add moons and adds remaining locations to post_gen OR just adds everything to post gen
 	if generate_sub_bodies:
-		generateRandomWeightedPlanets(new_planet, PA_chance_per_planet, missing_AO_chance_per_planet, missing_GL_chance_per_relevant_planet)
+		generateRandomWeightedPlanets(new_planet_id, PA_chance_per_planet, missing_AO_chance_per_planet, missing_GL_chance_per_relevant_planet)
+	else:
+		for iter in new_planet.metadata.get("iterations", 0):
+			post_gen_location_candidates.append([new_planet_id, iter])
 	pass
 func addRandomWeightedAsteroidBeltAtIteration(hook_identifier: int, i: int) -> int: # has a chance to fail if orbit_distance > belt_width
 	var hook = get_body_from_identifier(hook_identifier)

commit 0f866b6940bb8b94922034b52ea3979cb483a45b

Fixed SA spawn chance issues caused by d9fe237. SA_chance_per_candidate 0.01 -> 0.005. Iterations added to post_gen_location_candidates if not generating sub bodies are now the usual iterations count divided by 4. Misc changes. Added (now commented) code to test 100 star systems for factor(s) in game.gd _on_DEBUG_force_unexplored_system().

data/game/starSystemAPI.gd
@@ -651,7 +651,7 @@ func addRandomWeightedPlanetAtIteration(hook_identifier: int, i: int, remaining:
 		orbit_angle_change,
 		(radius / 109.1),
 		{"mass": (mass / 333000), "surface_color": color, "current_variation": planetBodyAPI.VARIATIONS.values().pick_random(), "layers": gas_layers_sum},
-		{"planet_classification": planet_classification, "planet_type": planet_type, "value": value, "iterations": (hook.metadata.get("iterations") / 2), "planetary_anomaly": has_planetary_anomaly, "planetary_anomaly_available": is_planetary_anomaly_available, "seed": randi(), "missing_AO": has_missing_AO, "missing_GL": has_missing_GL}
+		{"planet_classification": planet_classification, "planet_type": planet_type, "value": value, "iterations": int(hook.metadata.get("iterations") / 2), "planetary_anomaly": has_planetary_anomaly, "planetary_anomaly_available": is_planetary_anomaly_available, "seed": randi(), "missing_AO": has_missing_AO, "missing_GL": has_missing_GL}
 	)
 	var new_planet: planetBodyAPI = get_body_from_identifier(new_planet_id)
 	new_planet.rotation = deg_to_rad(global_data.get_randf(0,360))
@@ -660,7 +660,7 @@ func addRandomWeightedPlanetAtIteration(hook_identifier: int, i: int, remaining:
 	if generate_sub_bodies:
 		generateRandomWeightedPlanets(new_planet_id, PA_chance_per_planet, missing_AO_chance_per_planet, missing_GL_chance_per_relevant_planet)
 	else:
-		for iter in new_planet.metadata.get("iterations", 0):
+		for iter in int(new_planet.metadata.get("iterations") / 4):
 			post_gen_location_candidates.append([new_planet_id, iter])
 	pass
 func addRandomWeightedAsteroidBeltAtIteration(hook_identifier: int, i: int) -> int: # has a chance to fail if orbit_distance > belt_width

data/game/worldAPI.gd
@@ -133,7 +133,6 @@ func get_adjusted_SA_chance(advanced_scanning_unlocked: bool) -> float:
 		return SA_chance_per_candidate * advanced_scanning_multiplier
 	else:
 		return SA_chance_per_candidate
-
 func get_adjusted_PA_chance(advanced_scanning_unlocked: bool) -> float:
 	if advanced_scanning_unlocked:
 		return PA_chance_per_planet * advanced_scanning_multiplier

scenes/game/game.gd
@@ -31,7 +31,7 @@ func _ready():
 	
 	world = game_data.loadWorld()
 	if init_type == global_data.GAME_INIT_TYPES.TUTORIAL:
-		world = game_data.createWorld(25, 5, 25, 15, 5, 10, 25.0, 50.0, 0.01, 0.05, 0.25, 0.10)
+		world = game_data.createWorld(25, 5, 25, 15, 5, 10, 25.0, 50.0, 0.005, 0.05, 0.25, 0.10)
 		
 		dialogue_manager.dialogue_memory = world.dialogue_memory
 		
@@ -76,7 +76,7 @@ func _ready():
 		allow_quick_pause = true
 	
 	elif world == null or init_type == global_data.GAME_INIT_TYPES.NEW:
-		world = game_data.createWorld(25, 5, 25, 15, 5, 10, 25.0, 50.0, 0.01, 0.05, 0.25, 0.10)
+		world = game_data.createWorld(25, 5, 25, 15, 5, 10, 25.0, 50.0, 0.005, 0.05, 0.25, 0.10)
 		
 		dialogue_manager.dialogue_memory = world.dialogue_memory
 		
@@ -1526,8 +1526,37 @@ func _on_DEBUG_force_unexplored_system() -> void:
 	new.createAuxiliaryUnexplored(world.player.speed)
 	_on_switch_star_system(new)
 	_on_player_entering_system(new)
-	
 	_on_DEBUG_reveal_all_bodies()
+	
+	# test 100 star systems for factor(s) \/
+	#var star_hook_counts: Array[int] = []
+	#var sa_counts: Array[int] = []
+	#for i in 100:
+	#	var star_hook_count: int = 0
+	#	var sa_count: int = 0
+	#	
+	#	var new = _on_create_new_star_system()
+	#	new.createAuxiliaryUnexplored(world.player.speed)
+	#	for b in new.bodies:
+	#		if b.get_type() == starSystemAPI.BODY_TYPES.SPACE_ANOMALY:
+	#			sa_count += 1
+	#		if b is orbitBodyAPI:
+	#			if b.hook_identifier == new.get_first_star().get_identifier():
+	#				star_hook_count += 1
+	#	star_hook_counts.append(star_hook_count)
+	#	sa_counts.append(sa_count)
+	#
+	#var sum_func = func(accum, number):
+	#	return accum + number
+	#
+	#print(star_hook_counts)
+	#print(sa_counts)
+	#
+	#var analytics_avg_star_hook_count: float = float(star_hook_counts.reduce(sum_func, 0)) / star_hook_counts.size()
+	#var analytics_avg_sa_count: float = float(sa_counts.reduce(sum_func, 0)) / sa_counts.size()
+	#
+	#print(analytics_avg_star_hook_count)
+	#print(analytics_avg_sa_count)
 	pass
 
 func _on_DEBUG_max_character_standing() -> void: