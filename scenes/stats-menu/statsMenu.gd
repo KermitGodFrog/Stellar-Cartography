690205f954b715214f7192bacfe024a3ff933cf9
KermitGodFrog <kermitthegodfrog@gmail.com>
2026-09-04 14:46:01 +0800

    Mutations are now unlocked by statsMenu according to their schedules, however there is no UI to indicate the unlocks.

--- scenes/stats-menu/statsMenu.gd
+++ scenes/stats-menu/statsMenu.gd
@@ -47,6 +47,23 @@ func _on_open() -> void:
 		var new = statistic_scene.instantiate()
 		new.connect("ready", _on_statistic_instance_ready.bind(new, title))
 		stats_body_scroll.add_child(new)
+	
+	if init_type != INIT_TYPES.TUTORIAL:
+		var details_helper := game_data.loadUserDetails()
+		match init_type:
+			INIT_TYPES.DEATH:
+				details_helper.lose_condition_runs += 1
+				details_helper.total_runs += 1
+			INIT_TYPES.WIN:
+				details_helper.win_condition_runs += 1
+				details_helper.total_runs += 1
+		
+		try_unlock_mutations_add_items(details_helper)
+		
+		game_data.saveUserDetails(details_helper)
+	else:
+		#block out rewards panel w special text
+		pass
 	pass
 
 func _on_statistic_instance_ready(instance: Node, title: String) -> void:
@@ -58,3 +75,72 @@ func _on_statistic_instance_ready(instance: Node, title: String) -> void:
 		_: value_text = str(value)
 	instance.value_label.set_text(value_text)
 	pass
+
+func try_unlock_mutations_add_items(_details_helper: userDetailsHelper) -> void:
+	var pending_idx_unlocks: Array[worldAPI.MUTATION_ID] = []
+	var appropriate_schedule: Dictionary
+	var appropriate_runs_count: int
+	var runs_until_next_unlock: int = 0
+	
+	match init_type:
+		INIT_TYPES.DEATH:
+			appropriate_schedule.assign(game_data.MUTATION_UNLOCK_LOSE_SCHEDULE)
+			appropriate_runs_count = _details_helper.lose_condition_runs
+		INIT_TYPES.WIN:
+			appropriate_schedule.assign(game_data.MUTATION_UNLOCK_WIN_SCHEDULE)
+			appropriate_runs_count = _details_helper.win_condition_runs
+	
+	for runs in appropriate_schedule:
+		if runs <= appropriate_runs_count:
+			for idx in appropriate_schedule.get(runs):
+				if not is_mutation_unlocked(_details_helper, idx):
+					pending_idx_unlocks.append(idx)
+		elif runs > appropriate_runs_count:
+			runs_until_next_unlock = runs - appropriate_runs_count
+		
+	#^^^ if unlocks were missed due to an update, then the next run will result in ALL of the missed mutations being unlocked! quite immaculate if i do say so myself!  
+	
+	_details_helper.unlocked_mutations.append_array(pending_idx_unlocks)
+	
+	print_debug("DETAILS HELPER UNLOCKED MUTATIONS: ", _details_helper.unlocked_mutations)
+	print_debug("RUNS UNTIL NEXT UNLOCK: ", runs_until_next_unlock)
+	#now add items, including an item at the bottom showing the runs until next unlock \/
+	
+	for idx in pending_idx_unlocks:
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		pass
+	
+	
+	pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#misc
+
+func is_mutation_unlocked(_details_helper: userDetailsHelper, mutation_idx: worldAPI.MUTATION_ID) -> bool:
+	if _details_helper.unlocked_mutations.has(mutation_idx):
+		return true
+	return false
