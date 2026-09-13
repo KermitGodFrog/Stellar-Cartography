4649951b2bd2dfb35fde8503b53c2136b82d8ede
KermitGodFrog <kermitthegodfrog@gmail.com>
2026-09-13 14:21:38 +0800

    RP_TradeXP now plays ambience contextually. Fixed typo in playerMutinyWait trigger functions. 'player_any_characters_alive' is now added to queries by responseQuery populateWithPlayerData().

--- data/dialogue-control/query.gd
+++ data/dialogue-control/query.gd
@@ -80,6 +80,8 @@ func populateWithPlayerData(player: playerAPI) -> void:
 		add("player_%s_standing" % characterAPI.OCCUPATIONS.find_key(character.get_occupation()), character.get_standing())
 		add("player_%s_xp" % characterAPI.OCCUPATIONS.find_key(character.get_occupation()), character.xp)
 		add("player_%s_initiative_xp" % characterAPI.OCCUPATIONS.find_key(character.get_occupation()), character.initiative_xp)
+	
+	add("player_any_characters_alive", player.characters.all(func(c): return c.is_alive()) == true)
 	pass
 
 func populateWithSystemData(system: starSystemAPI) -> void:
