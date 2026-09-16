ba673038584c3c0313387b01c11cf4f0db4b8dce
KermitGodFrog <kermitthegodfrog@gmail.com>
2026-09-16 12:06:30 +0800

    Combined all create_menu mutations_panel issue symbols into one instance. The issue symbol now appears when there is unspent mutation points.

--- scenes/create-menu/mutations_panel.gd
+++ scenes/create-menu/mutations_panel.gd
@@ -8,16 +8,17 @@ var current_difficulty: game_data.DIFFICULTY = game_data.DIFFICULTY.NORMAL: #upd
 	set(value):
 		current_difficulty = value
 		emit_signal("mutation_items_changed")
-
 @onready var uninstalled_list = $margin/scroll/UNINSTALLED/uninstalled_list
 @onready var installed_list = $margin/scroll/INSTALLED/installed_list
 @onready var points_counter = $margin/scroll/points_scroll/points_counter
 
-@onready var issue_symbol_points = $margin/scroll/points_scroll/issue_symbol
-@onready var issue_symbol_quantity = $margin/scroll/title_label/issue_symbol
+@onready var issue_symbol = $margin/scroll/points_scroll/issue_symbol
 
 @onready var mutation_item_scene = preload("uid://dte1ssronei0")
 @onready var station_repair = preload("uid://dha2d3lx22sd1")
+
+@onready var issue_dpi = preload("uid://c81t88q8femj2")
+@onready var warning_dpi = preload("uid://dobhjjypnoqxr")
 
 func _ready() -> void:
 	uninstalled_list.connect("child_order_changed", _on_list_child_order_changed)
@@ -35,10 +36,19 @@ func _on_list_child_order_changed() -> void:
 func _on_mutation_items_changed() -> void:
 	if points_counter != null:
 		points_counter.set_text("CURRENT MUTATION POINTS: %d" % get_current_points())
-	if issue_symbol_points != null and installed_list != null:
-		issue_symbol_points.visible = get_current_points() < 0
-	if issue_symbol_quantity != null and installed_list != null:
-		issue_symbol_quantity.visible = get_installed_mutation_items().size() > 5
+	if issue_symbol != null and installed_list != null:
+		var new_tooltip: String = String()
+		if get_current_points() > 0:
+			new_tooltip += "* [color=yellow]Unspent mutation points.[/color]\n\n"
+			issue_symbol.set_texture(warning_dpi)
+		else:
+			issue_symbol.set_texture(issue_dpi)
+		if get_current_points() < 0:
+			new_tooltip += "* [color=red]Mutation points can't be below zero (0).[/color]\n\n"
+		if get_installed_mutation_items().size() > 5:
+			new_tooltip += "* [color=red]Only five (5) mutations can be installed at the same time.[/color]\n\n"
+		issue_symbol.set_tooltip_text(new_tooltip)
+		issue_symbol.visible = get_current_points() < 0 or get_current_points() > 0 or get_installed_mutation_items().size() > 5
 	pass
 
 
